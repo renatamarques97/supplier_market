7c9877bd976a2c9492902a4c6a9e7062db09aa70
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-07-28 16:12:11 -0300

    routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :people, controllers: {
-    sessions:      'people/sessions',
-    password:      'people/passwords',
+  # devise_for :people, controllers: {
+  #   sessions:      'people/sessions',
+  #   password:      'people/passwords',
+  #   registrations: 'clients/registrations'
+  # }
+
+  devise_for :clients, controllers: {
+    sessions:      'clients/sessions',
+    password:      'clients/passwords',
     registrations: 'clients/registrations'
   }
+
+  devise_for :providers, controllers: {
+    sessions:      'providers/sessions',
+    password:      'providers/passwords',
+    registrations: 'providers/registrations'
+  }
+
+  devise_for :adm, controllers: {
+    sessions:      'adm/sessions',
+    password:      'adm/passwords'
+  }
+
+  root to: "home#index"
 end
