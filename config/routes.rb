050619cf1b124d6de039dab4bc02c2d6c1d5776b
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-10 20:34:14 -0300

    remove: routes rails

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,22 @@ Rails.application.routes.draw do
   #   registrations: 'clients/registrations'
   # }
 
-  devise_for :clients, controllers: {
-    sessions:      'clients/sessions',
-    password:      'clients/passwords',
-    registrations: 'clients/registrations'
-  }
-
-  devise_for :providers, controllers: {
-    sessions:      'providers/sessions',
-    password:      'providers/passwords',
-    registrations: 'providers/registrations'
-  }
-
-  devise_for :adm, controllers: {
-    sessions:      'adm/sessions',
-    password:      'adm/passwords'
-  }
+  # devise_for :clients, controllers: {
+  #   sessions:      'clients/sessions',
+  #   password:      'clients/passwords',
+  #   registrations: 'clients/registrations'
+  # }
+  #
+  # devise_for :providers, controllers: {
+  #   sessions:      'providers/sessions',
+  #   password:      'providers/passwords',
+  #   registrations: 'providers/registrations'
+  # }
+  #
+  # devise_for :adm, controllers: {
+  #   sessions:      'adm/sessions',
+  #   password:      'adm/passwords'
+  # }
 
   root to: "home#index"
 end
