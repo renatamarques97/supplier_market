614aab8f247d017c364c8baa8a8a82b989cd5175
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-11 17:58:51 -0300

    add end point "check_logged"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def check_logged
+    if current_client
+      render json: current_client 
+    elsif current_provider
+      render json: current_provider
+    elsif current_adm
+      render json: current_adm
+    end
+  end
 end
