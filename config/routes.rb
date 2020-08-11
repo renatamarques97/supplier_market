add6c41381bfbd3bb84b14e4bc417083437d1e11
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-11 17:59:57 -0300

    add routes for login/sign up clients and providers

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,23 @@ Rails.application.routes.draw do
   #   registrations: 'clients/registrations'
   # }
 
-  # devise_for :clients, controllers: {
-  #   sessions:      'clients/sessions',
-  #   password:      'clients/passwords',
-  #   registrations: 'clients/registrations'
-  # }
-  #
-  # devise_for :providers, controllers: {
-  #   sessions:      'providers/sessions',
-  #   password:      'providers/passwords',
-  #   registrations: 'providers/registrations'
-  # }
-  #
+  devise_for :clients, controllers: {
+    sessions:      'clients/sessions',
+    password:      'clients/passwords',
+    registrations: 'clients/registrations'
+  }
+
+  devise_for :providers, controllers: {
+    sessions:      'providers/sessions',
+    password:      'providers/passwords',
+    registrations: 'providers/registrations'
+  }
+  
   # devise_for :adm, controllers: {
   #   sessions:      'adm/sessions',
   #   password:      'adm/passwords'
   # }
 
   root to: "home#index"
+  get '/check_logged', to: 'home#check_logged'
 end
