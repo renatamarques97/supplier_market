f3e213edcd86d06fae9e8c599626c4be389bd704
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-25 10:09:12 -0300

    indent factory providers

--- spec/factories/providers.rb
+++ spec/factories/providers.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :provider do
-    name { FFaker::Name.name }
+    name      { FFaker::Name.name }
     telephone { FFaker::PhoneNumber.phone_number }
-    cnpj { FFaker::IdentificationBR.cnpj }
-    email { FFaker::Internet.email }
-    password { FFaker::Internet.password }
-    provider { true }
-    client { false }
+    cnpj      { FFaker::IdentificationBR.cnpj }
+    email     { FFaker::Internet.email }
+    password  { FFaker::Internet.password }
+    provider  { true }
+    client    { false }
   end
 end
