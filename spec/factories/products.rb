f48eaba4674f73efa5db30a4ed2e72ff6bf7975f
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-08-25 10:11:21 -0300

    indent factory products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :product do
-    name { FFaker::Product.product }
+    name        { FFaker::Product.product }
     description { FFaker::Lorem.sentences(5) }
-    dimensions { "" }
-    weight { FFaker::Random.rand(4.0) }
-    quantity { FFaker::Random.rand(4) }
+    dimensions  { "" }
+    weight      { FFaker::Random.rand(4.0) }
+    quantity    { FFaker::Random.rand(4) }
     person_type { "Person" }
     # person_id { create(:provider).id }
     # association :person
