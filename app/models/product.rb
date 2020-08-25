d698974864f990e862eb7472c1bc70b36f9fb606
Renata Marques <renata.nrmarques@gmail.com>
2020-08-24 22:56:45 -0300

    add: product model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :person, polymorphic: true
+
+  validates :person_id, presence: true
+  validates :person_type, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe "validations" do
     it { is_expected.to belong_to(:person) }
-    # it { is_expected.to validate_presence_of(:person_id) }
+    it { is_expected.to validate_presence_of(:person_id) }
+    it { is_expected.to validate_presence_of(:person_type) }
   end
 end
