b70c53aeb026a8a51d663d838c7faac6bab73f5d
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-16 13:17:34 -0300

    add shopping cart page

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,4 +1,19 @@
 class ShoppingCartController < ApplicationController
+  def index
+    @shopping_cart = []
+    return unless session[:shopping_cart]
+
+    @shopping_cart = session[:shopping_cart].inject([]) do |array, shopping_cart|
+      product = Product.find_by_id(shopping_cart["product_id"])
+
+      array << {
+        product: product, 
+        quantity: shopping_cart["quantity"]
+      }
+      array
+    end
+  end
+
   def add_to_cart
     product_id = params[:product_id]
     session[:shopping_cart] = [] unless session[:shopping_cart]
