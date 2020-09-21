94578696d3497fea7f038b31600535b36b2ab752
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-21 11:32:06 -0300

    add: set_provider in control_pane_controller

--- app/controllers/adms/control_panel_controller.rb
+++ app/controllers/adms/control_panel_controller.rb
@@ -1,25 +1,24 @@
 class Adms::ControlPanelController < ApplicationController
+  before_action :set_provider, only: [:accept_provider, :reject_provider]
+ 
   def index
     @people = Person.where(admin: false, provider: false, client:false)
   end
 
   def accept_provider
-    provider_id = params[:provider_id]
-
-    provider = Person.find(provider_id)
-
-    provider.update(provider: true)
+    @_provider.update(provider: true)
     redirect_to "/adms/control_panel"
   end
 
   def reject_provider
-    provider_id = params[:provider_id]
-
-    provider = Person.find(provider_id)
-
-    provider.destroy
+    @_provider.destroy
     redirect_to "/adms/control_panel"
   end
 
+  private
+
+  def set_provider
+    @_provider = Person.find_by_id(params[:product_id])
+  end
 
 end
