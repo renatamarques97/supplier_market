fbdee7a14ce5c7b98143272b09af4a600325ff11
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-20 23:15:38 -0300

    add: identation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,15 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :height, :width, :length, :weight, :quantity)
+    params.require(:product).permit(
+      :name,
+      :description,
+      :height,
+      :width,
+      :length,
+      :weight,
+      :price,
+      :quantity
+    )
   end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,52 +1,51 @@
 class PurchasesController < ApplicationController
-    before_action :set_purchase, only: [:show, :update, :destroy]
+  before_action :set_purchase, only: [:show, :update, :destroy]
   
-    # GET /purchases
-    def index
-      @purchases = Purchase.all
+  # GET /purchases
+  def index
+    @purchases = Purchase.all
+  end
+  
+  # GET /purchases/id
+  def show
     end
   
-    # GET /purchases/id
-    def show
-    end
-  
-    # GET /purchases/new
-    def new
-      @purchase = Purchase.new
-    end
+  # GET /purchases/new
+  def new
+    @purchase = Purchase.new
+  end
   
     # POST /purchases
-    def create
-      @purchase = Purchase.new(purchase_params)
+  def create
+    @purchase = Purchase.new(purchase_params)
   
-      respond_to do |format|
-        if @purchase.save
-          format.html { redirect_to @purchase, notice: 'Purchase was successfully created.' }
-          format.json { render :show, status: :created, location: @purchase }
-        else
-          format.html { render :new }
-          format.json { render json: @purchase.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @purchase.save
+        format.html { redirect_to @purchase, notice: 'Purchase was successfully created.' }
+        format.json { render :show, status: :created, location: @purchase }
+      else
+        format.html { render :new }
+        format.json { render json: @purchase.errors, status: :unprocessable_entity }
       end
     end
+  end
   
     # DELETE /purchases/id
-    def destroy
-      @purchase.destroy
-      respond_to do |format|
-        format.html { redirect_to purchases_url, notice: 'Purchase was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+  def destroy
+    @purchase.destroy
+    respond_to do |format|
+      format.html { redirect_to purchases_url, notice: 'Purchase was successfully destroyed.' }
+      format.json { head :no_content }
     end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_purchase
-        @purchase = Purchase.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def purchase_params
-        params.require(:purchase).permit(:price, :discount, :final_price, :shipping)
-      end
   end
+  
+  private
+
+  def set_purchase
+    @purchase = Purchase.find(params[:id])
+  end
+  
+  def purchase_params
+    params.require(:purchase).permit(:price, :discount, :final_price, :shipping)
+  end
+end
