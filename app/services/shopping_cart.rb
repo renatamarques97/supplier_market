6d793890058fc1abcbecfa1a90aab5a1dc46d4b4
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-21 11:17:02 -0300

    refactor(ShoppingCartService): always convert hash keys to symbols

--- app/services/shopping_cart.rb
+++ app/services/shopping_cart.rb
@@ -1,6 +1,8 @@
 class ShoppingCart
   class << self
     def add_to_cart(cart, product_id)
+      cart = convert_to_symbol(cart)
+
       cart << {
         product_id: product_id,
         quantity: 1
@@ -10,19 +12,22 @@ class ShoppingCart
     end
 
     def remove_one_from_cart(cart, product_id)
-      cart.find { |item| item["product_id"] == product_id ? item["quantity"]-=1 : nil }
-      cart.reject! { |item| item["quantity"].zero? }
+      cart = convert_to_symbol(cart)
+      cart.find    { |item| item[:product_id] == product_id ? item[:quantity]-=1 : nil }
+      cart.reject! { |item| item[:quantity].zero? }
     end
 
     def remove_all_from_cart(cart, product_id)
-      cart.reject! { |item| item["product_id"] == product_id }
+      cart = convert_to_symbol(cart)
+      cart.reject! { |item| item[:product_id] == product_id }
     end
 
     def cart_formatted(cart)
+      cart = convert_to_symbol(cart)
       cart.inject([]) do |newCart, shopping_cart|
         newCart << {
-          product: find_product(shopping_cart["product_id"]), 
-          quantity: shopping_cart["quantity"]
+          product: find_product(shopping_cart[:product_id]), 
+          quantity: shopping_cart[:quantity]
         }
       end
     end
@@ -30,11 +35,15 @@ class ShoppingCart
     private
 
     def item_already_added?(cart, product_id)
-      cart.find{ |item| item["product_id"] == product_id ? item["quantity"]+=1 : nil}
+      cart.find{ |item| item[:product_id] == product_id ? item[:quantity]+=1 : nil}
     end
 
     def find_product(product_id)
       Product.find_by_id(product_id)
+    end
+
+    def convert_to_symbol(cart)
+      cart.map(&:symbolize_keys) 
     end
   end
 end
