b878c69b2ff6d1deaa08a82ed7ab494019a61617
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-09-21 21:01:28 -0300

    fix: remove redirect login admin

--- app/controllers/adms/sessions_controller.rb
+++ app/controllers/adms/sessions_controller.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 class Adms::SessionsController < Devise::SessionsController
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    redirect_to "/adms/control_panel"
-  end
-
 end
