da27c7c093dd364b6149735f4f5575680afbf05d
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-09-22 18:47:21 -0300

    refactor(shopping cart): cart is converted to a valid hash and is an instance variable

--- app/services/shopping_cart.rb
+++ app/services/shopping_cart.rb
@@ -1,31 +1,33 @@
 class ShoppingCart
   class << self
     def add_to_cart(cart, product_id)
-      cart = convert_to_symbol(cart)
+      convert_cart(cart)
 
-      cart << {
+      @_cart << {
         product_id: product_id,
         quantity: 1
-      } unless item_already_added?(cart, product_id)
+      } unless item_already_added?(@_cart, product_id)
 
-      cart
+      @_cart
     end
 
     def remove_one_from_cart(cart, product_id)
-      cart = convert_to_symbol(cart)
+      convert_cart(cart)
 
-      cart.find    { |item| item[:product_id] == product_id ? item[:quantity]-=1 : nil }
-      cart.reject { |item| item[:quantity].zero? }
+      @_cart.find   { |item| item[:product_id] == product_id ? item[:quantity]-=1 : nil }
+      @_cart.reject { |item| item[:quantity].zero? }
     end
 
     def remove_all_from_cart(cart, product_id)
-      cart = convert_to_symbol(cart)
-      cart.reject { |item| item[:product_id] == product_id }
+      convert_cart(cart)
+
+      @_cart.reject { |item| item[:product_id] == product_id }
     end
 
     def cart_formatted(cart)
-      cart = convert_to_symbol(cart)
-      cart.inject([]) do |newCart, shopping_cart|
+      convert_cart(cart)
+
+      @_cart.inject([]) do |newCart, shopping_cart|
         newCart << {
           product: find_product(shopping_cart[:product_id]), 
           quantity: shopping_cart[:quantity]
@@ -41,6 +43,10 @@ class ShoppingCart
 
     def find_product(product_id)
       Product.find_by_id(product_id)
+    end
+
+    def convert_cart(cart)
+      @_cart = convert_to_symbol(cart)
     end
 
     def convert_to_symbol(cart)
