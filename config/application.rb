be1421f11bffaeeacacd155db23e0c08493f6949
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-02 13:22:25 -0300

    add: separate en and pt locale folders

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module SuplierMarket
 
     config.i18n.available_locales = [:en, :pt]
     config.i18n.default_locale = :pt
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
