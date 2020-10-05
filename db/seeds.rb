78170904a48e7b8e918e9d40f7c8d5c3ba5f02f6
Renata Marques <renata.nrmarques@gmail.com>
2020-10-05 09:59:46 -0300

    Layout/home page (#22)
    * add: color pallete
    
    * add: cart icon
    
    * add: seeds for products
    
    * add: forms with new color pallete
    
    * add: better messages for forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+provider = Person.last
+
+20.times do
+  Product.create(
+    name: FFaker::Product.product,
+    description: FFaker::Product.model,
+    height: FFaker::Random.rand(10..80),
+    width: FFaker::Random.rand(10..80),
+    length: FFaker::Random.rand(10..80),
+    price: FFaker::Random.rand(10.0..80.0),
+    weight: FFaker::Random.rand(1.0..8.0).round(2),
+    quantity: FFaker::Random.rand(20..80),
+    person_type: "Provider",
+    person: provider
+  )
+end
