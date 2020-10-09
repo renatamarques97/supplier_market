1ad0aa084a05268b6c68ee9aaa2ff2ab111ef56b
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-09 12:25:01 -0300

    fix: provider mailer spec

--- spec/mailers/provider_spec.rb
+++ spec/mailers/provider_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe ProviderMailer, type: :mailer do
     let!(:mail)     { ProviderMailer.with(provider: provider).waiting_administrator_confirmation }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Mercado Fornecedor - Castro Realizado")
+      expect(mail.subject).to eq("Mercado Fornecedor - Cadastro Realizado")
       expect(mail.to).to eq([provider.email])
       expect(mail.from).to eq(["mercado.fornecedor@hotmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Cadastro Realizado - Mercado Fornecedor")
+      expect(mail.body.encoded).to match("Cadastro Realizado no Mercado Fornecedor")
     end
   end
 
