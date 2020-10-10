447457a06d836c5a5dea75b99d6b61e3e7de96d8
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-09 21:52:03 -0300

    refactor(provider mailer spec): added "." to describe and "let" changed to "before do"

--- spec/mailers/provider_spec.rb
+++ spec/mailers/provider_spec.rb
@@ -1,19 +1,21 @@
 require "rails_helper"
 
 RSpec.describe ProviderMailer, type: :mailer do
-  describe "waiting_administrator_confirmation" do
+  describe "#waiting_administrator_confirmation" do
     let!(:provider) { create(:provider) }
-    let!(:mail)     { ProviderMailer.with(provider: provider).waiting_administrator_confirmation }
 
+    before(:each) do
+      @mail = ProviderMailer.with(provider: provider).waiting_administrator_confirmation
+    end
+  
     it "renders the headers" do
-      expect(mail.subject).to eq("Mercado Fornecedor - Cadastro Realizado")
-      expect(mail.to).to eq([provider.email])
-      expect(mail.from).to eq(["mercado.fornecedor@hotmail.com"])
+      expect(@mail.subject).to eq("Mercado Fornecedor - Cadastro Realizado")
+      expect(@mail.to).to eq([provider.email])
+      expect(@mail.from).to eq(["mercado.fornecedor@hotmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Cadastro Realizado no Mercado Fornecedor")
+      expect(@mail.body.encoded).to match("Cadastro Realizado no Mercado Fornecedor")
     end
   end
-
 end
