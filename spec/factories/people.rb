9f43005b3bd38c42de9bde788f26bf89970c4793
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-10-09 22:26:29 -0300

    add(person model spec): 'active_for_authentication?' spec

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,10 +1,16 @@
 FactoryBot.define do
   factory :person do
-    name      { FFaker::Name.name }
+    name      { FFaker::Name.name                }
     telephone { FFaker::PhoneNumber.phone_number }
-    cnpj      { FFaker::IdentificationBR.cnpj }
-    email     { FFaker::Internet.email }
-    password  { FFaker::Internet.password }
+    cnpj      { FFaker::IdentificationBR.cnpj    }
+    email     { FFaker::Internet.email           }
+    password  { FFaker::Internet.password        }
+
+    trait :not_actived do
+      admin    { false }
+      client   { false }
+      provider { false }
+    end
   end
 
   factory :client, class: Client, parent: :person do
@@ -14,5 +20,10 @@ FactoryBot.define do
   factory :provider, class: Provider, parent: :person do
     client   { false }
     provider { true  }
+  end
+
+  factory :adm, class: Adm, parent: :person do
+    client { false }
+    admin  { true  }
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,10 +2,49 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   describe "validations" do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:name)      }
+    it { is_expected.to validate_presence_of(:email)     }
     it { is_expected.to validate_presence_of(:telephone) }
-    it { is_expected.to validate_presence_of(:cnpj) }
+    it { is_expected.to validate_presence_of(:cnpj)      }
+
     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  end
+
+  describe "#active_for_authentication?" do
+    let(:client)   { create(:client)   }
+    let(:provider) { create(:provider) }
+    let(:adm)      { create(:adm)      }
+
+    let(:client_not_actived)   { create(:client, :not_actived)   }
+    let(:provider_not_actived) { create(:provider, :not_actived) }
+    let(:adm_not_actived)      { create(:adm, :not_actived)      }
+
+    context "return must be true" do
+      it "client" do
+        expect(client.active_for_authentication?).to be_truthy
+      end
+
+      it "provider" do
+        expect(provider.active_for_authentication?).to be_truthy
+      end
+
+      it "admin" do
+        expect(adm.active_for_authentication?).to be_truthy
+      end
+    end
+
+    context "return must be false" do
+      it "client" do
+        expect(client_not_actived.active_for_authentication?).to be_falsey
+      end
+
+      it "provider" do
+        expect(provider_not_actived.active_for_authentication?).to be_falsey
+      end
+
+      it "admin" do
+        expect(adm_not_actived.active_for_authentication?).to be_falsey
+      end
+    end
   end
 end
