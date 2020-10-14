d041f55c980019d714154c267a8ae618f117f510
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2020-10-14 20:59:23 -0300

    Fix: client edit page and provider edit page

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
 
 class Clients::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:create]
+  before_action :create_configure_permitted_parameters, only: [:create]
+  before_action :update_configure_permitted_parameters, only: [:update]
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |c| 
+  def create_configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |c|
       c.permit(
-        :email, 
-        :password, 
+        :email,
+        :password,
         :password_confirmation,
-        :name, 
-        :telephone, 
+        :name,
+        :telephone,
         :cnpj
+      )
+    end
+  end
+
+  def update_configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |c|
+      c.permit(
+        :email,
+        :password,
+        :password_confirmation,
+        :current_password,
+        :name,
+        :telephone
       )
     end
   end
